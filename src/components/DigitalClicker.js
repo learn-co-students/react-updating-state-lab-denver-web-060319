a691d5d973eec09f3a01e5b582b01d59ff01ef38
Devin Arnold <dbarnold321@gmail.com>
2019-08-07 14:25:38 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react';
+
+export default class DigitalClicker extends Component{
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0,
+        };
+    }
+
+    increment = () => {
+       this.setState(times =>({
+           timesClicked: times.timesClicked+1
+       }))
+    }
+
+    render(){
+        return (
+            <div className = "digital-clicker">
+                <button onClick = {this.increment}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react';
+
+export default class YouTubeDebugger extends Component{
+    constructor(){
+        super()
+        this.state ={
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+        }
+
+        bitratechanger = () =>{
+            this,this.setState({
+                settings:{
+                    ...this.state.settings, 
+                    bitrate: 12
+                }
+            })
+        }
+
+        reschanger = () =>{
+            this,this.setState({
+                settings:{
+                    ...this.state.settings, 
+                    video: {
+                        ...this.state.settings.video,
+                    resolution: '720p'
+                    }
+                }
+            })
+        }
+
+
+
+    
+render(){
+
+    return(
+        <div>
+            <button onClick = {this.bitratechanger} className = 'bitrate'>change bitrate
+            </button>
+
+            <button onClick = {this.reschanger} className = 'resolution'>change resolution
+            </button>
+        </div>
+    )
+}
+}
