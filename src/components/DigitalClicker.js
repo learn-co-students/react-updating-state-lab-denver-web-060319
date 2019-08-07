5cd2ad00d77ee5540a94fbc648ec98aa4746aef8
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-08-07 16:16:07 -0600

    Update state using setState()

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component{
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  incrementTimesClicked = () => {
+    let newTimesClicked = this.state.timesClicked + 1
+    this.setState ({
+      timesClicked: newTimesClicked
+    })
+  }  
+
+  render() {
+    return(
+      <button id='digitalClicker' onClick={this.incrementTimesClicked}>{this.state.timesClicked}</button>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component{
+  constructor(){
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+
+  changeBitrate = () => {
+    // let updatedState = this.state;
+    // updatedState.settings.bitrate = 12;
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+      // updatedState
+    })
+  }
+
+  changeResolution = () => {
+    // let updatedState = this.state;
+    // updatedState.settings.video.resolution = "720p"
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+      // updatedState
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <button className="bitrate" onClick={this.changeBitrate}>Bitrate: {this.state.settings.bitrate}</button>
+        <button className="resolution" onClick={this.changeResolution}>Resolution: {this.state.settings.video.resolution}</button>
+      </div>
+    )
+  }
+}
